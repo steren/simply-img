ec9c5c2d02da3c234039db49d9c14aec3c09a89e
Steren <steren.giannini@gmail.com>
2023-02-21 23:19:48 +0000

    Add support for DPR
    Fix #3

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import { runInNewContext } from 'vm';
 
 const readFile = fs.promises.readFile;
 
@@ -11,36 +12,72 @@ async function serveIndex(req, res) {
   const data = await readFile('index.html');
   res.setHeader('Content-Type', 'text/html');
   
+  // Tell the client that the server can use device pixel ratio
+  res.setHeader('Accept-CH', 'DPR');
+
   console.log(`response headers sent by "${req.url}":`);
   console.log(JSON.stringify(res.getHeaders(), null, 2));
   res.end(data);
+}
+
+// Return the image to serve based on a ste of options (content type,pixel ratio, etc.)
+function pickFile(image, options) {
+  const { contentType, dpr } = options;
+
+  let extension;
+  if(contentType === 'image/avif') {
+    extension = 'avif';
+  } else if(contentType === 'image/webp') {
+    extension = 'webp';
+  } else {
+    extension = 'jpg';
+  }
+
+  let pixelRatio = 1;
+  if(dpr > 1) {
+    pixelRatio = 2;
+  }
+
+  return `images/${image}-${pixelRatio}x.${extension}`;
 }
 
 async function serveImage(req, res) {
   console.log(`request headers received by "${req.url}":`);
   console.log(JSON.stringify(req.headers, null, 2));
 
+  res.setHeader('Vary', 'Accept, DPR');
+
+  let contentType;
+  let dpr;
+
   const acceptHeader = req.headers.accept;
   const image = path.basename(req.url);
-  let file;
-  let contentType;
+  
 
   if (acceptHeader && acceptHeader.includes('image/avif')) {
-    file = `images/${image}.avif`;
     contentType = 'image/avif';
   } else if (acceptHeader && acceptHeader.includes('image/webp')) {
-    file = `images/${image}.webp`;
     contentType = 'image/webp';
   } else {
-    file = `images/${image}.jpg`;
     contentType = 'image/jpeg';
   }
-
-  res.setHeader('Vary', 'Accept');
-
-  console.log(`Serving image: ${contentType}`);
-
   res.setHeader('Content-Type', contentType);
+  console.log(`Image Content-Type served: ${contentType}`);
+
+  
+  if(req.headers.dpr) {
+    console.log(`Client Device Pixel Ratio: ${req.headers.dpr}`);
+
+    dpr = Math.ceil(parseFloat(req.headers.dpr));
+
+    console.log(`Image Device Pixel Ratio served: ${dpr}`);
+    res.setHeader('Content-DPR', dpr);
+  }
+
+  const file = pickFile(image, { contentType, dpr });
+
+
+  console.log(`Serving file: ${file}`);
 
   try {
     const data = await readFile(file);
